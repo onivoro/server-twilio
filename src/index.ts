81006ed15d3854b7840b463208373a2481bee031
icedlee337 <lee.e.norris@gmail.com>
2024-10-13 21:59:38 -0500

    chore: export Twilio

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/classes/server-twilio-config.class';
 export * from './lib/services/twilio.service';
 
 export * from './lib/api-twilio.module';
+
+export { Twilio } from 'twilio';
